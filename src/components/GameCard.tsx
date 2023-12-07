253d5ecdbccb77c61529e35486c305ab15f84120
lahlimy_10 <aziz.lahlimy@gmail.com>
2023-12-07 13:23:05 +0100

    add Card Skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,26 +1,28 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
-import { Game } from '../hooks/useGames'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} />
+    <Card width='300px' borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-
-      <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms?.map((p) => p.platform)}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
